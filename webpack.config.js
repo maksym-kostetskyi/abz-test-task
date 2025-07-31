44f1e4af82837c3e687bbd1c08084ced46a7e833
Maksym Kostetskyi <mkost996@gmail.com>
2025-07-31 11:25:44 +0300

    Add GitHub Pages deployment setup and favicon fixes

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: process.env.NODE_ENV === 'production' ? '/abz-test-task/' : '/',
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
